3c211e6b0a72634732dee5fcb7b1658d122baa70
Brent Rainwater <brentvsmaximvs@gmail.com>
2020-05-13 17:45:17 -0400

    upload changes
    upload changes to todo list

--- todos/app.py
+++ todos/app.py
@@ -1,15 +1,20 @@
-from flask import Flask, render_template, request, url_for, redirect, jsonify
+from flask import Flask, request, url_for, render_template, redirect, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand
+import sys
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:b1j2j3j4@localhost:5432/todoapp';
+
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # models
 class Todo(db.Model):
     __tablename__ = "todos"
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
 
 def __repr__(self):
@@ -19,14 +24,29 @@ db.create_all()
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
-    description = request.get_json()['description']
-    todo = Todo(description=description)
-    db.session.add(todo)
-    db.session.commit()
-    return jsonify({
-        'description': todo.description
-    })
+    error = False
+    body = {}
+    try:
+        description = request.get_json()['description']
+        todo = Todo(description=description)
+        db.session.add(todo)
+        db.session.commit()
+        body['description'] = todo.description
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if not error:
+        return jsonify(body)
 
+
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+    try:
+        completed = request.get_json()['completed']
+        
 
 @app.route('/')
 def index():
